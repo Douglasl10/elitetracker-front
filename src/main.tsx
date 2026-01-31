5063aa34a5209ca7653427696ad45a318584e329
Douglas <ftsgmr@gmail.com>
2026-01-31 20:59:49 -0300

    refactor: move router provider from app to main entry point
    Simplify app component by removing routing logic and returning null. This centralizes routing setup in the main entry file for better separation of concerns and aligns with typical React app bootstrapping patterns.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 
-import "./styles/global.css"
-import { App } from './app'
+import "./styles/global.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
