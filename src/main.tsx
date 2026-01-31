0326e9ec49d97f375395ee64f695c4464dfa212c
Douglas <ftsgmr@gmail.com>
2026-01-31 20:45:32 -0300

    up

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
 
 import "./styles/global.css"
 import { App } from './app'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,26 +1,13 @@
+import { createHashRouter } from "react-router-dom";
+import Login from "../pages/login";
+import { PrivateRoutes } from "./private-routes";
+import Habits from "../pages/habits";
+import AuthRedirect from "../pages/auth";
+import { Focus } from "../pages/focus";
 
-import { createBrowserRouter } from 'react-router-dom';
-import Login from '../pages/login';
-import Habits from '../pages/habits';
-import Autenticacao from '../pages/auth';
-import { PrivateRoutes } from './private-routes';
-import { Focus } from '../pages/focus';
-
-export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Login/>
-    },
-    {
-        path: '/habits',
-        element: <PrivateRoutes component={<Habits />} />
-    },
-    {
-        path: '/autenticacao',
-        element: <Autenticacao />
-    },
-    {
-        path: '/focus',
-        element: <PrivateRoutes component={<Focus />} />
-    },
+export const router = createHashRouter([
+  { path: "/", element: <Login /> },
+  { path: "/habits", element: <PrivateRoutes component={<Habits />} /> },
+  { path: "/autenticacao", element: <AuthRedirect /> },
+  { path: "/focus", element: <PrivateRoutes component={<Focus />} /> },
 ]);
