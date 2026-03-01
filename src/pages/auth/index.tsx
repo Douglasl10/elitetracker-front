2f71ff002ebbb33a80ddb4d61ee268ca6d89de4a
Douglas <odouglas918@gmail.com>
2026-03-01 17:08:58 -0300

    up

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,26 +1,21 @@
 import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { useUser } from "../../hooks/use-user";
+// import { useUser } from "../../hooks/use-user"; // removido
 
 export default function AuthRedirect() {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
-  const { userData } = useUser();
 
   useEffect(() => {
     const token = searchParams.get("token");
 
     if (token) {
-      // salva no localStorage (sem espaço na chave)
       localStorage.setItem(
         import.meta.env.VITE_LOCALSTORAGE_KEY + ":userData",
         JSON.stringify({ token })
       );
-
-      // redireciona para a área logada
       navigate("/habits"); // ou "/focus"
     } else {
-      // se não houver token, volta para a home
       navigate("/");
     }
   }, [searchParams, navigate]);
