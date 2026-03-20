fa1d9dd49336e2377dc391acb87f23ba45012f41
Douglas <odouglas918@gmail.com>
2026-03-19 22:51:04 -0300

    feat: add MantineProvider and import core styles
    Integrate Mantine UI library for consistent styling and components. The provider is configured with dark mode as the default color scheme.

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { UserProvider } from "./hooks/use-user";
+import { MantineProvider } from "@mantine/core";
 
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <UserProvider>
-      <RouterProvider router={router} />
-    </UserProvider>
+    <MantineProvider defaultColorScheme="dark">
+      <UserProvider>
+        <RouterProvider router={router} />
+      </UserProvider>
+    </MantineProvider>
   </React.StrictMode>
 );
